7048b682deb780407c395ffe6b0f5511f0c21340
Charle <carlos.itbone@gmail.com>
2021-11-11 19:43:43 -0300

    Update

--- src/main/java/com/crud/rest/exceptions/NotFoundException.java
+++ src/main/java/com/crud/rest/exceptions/NotFoundException.java
@@ -2,6 +2,9 @@ package com.crud.rest.exceptions;
 
 public class NotFoundException extends RuntimeException{
 
+
+	private static final long serialVersionUID = 1L;
+
 	public NotFoundException() {
 		super();
 	}
