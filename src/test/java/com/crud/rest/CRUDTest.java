674e05981b25433d6f3a5e294058f8a0e76df726
Charle <carlos.itbone@gmail.com>
2021-11-12 00:30:02 -0300

    Update Test

--- src/test/java/com/crud/rest/CRUDTest.java
+++ src/test/java/com/crud/rest/CRUDTest.java
@@ -1,51 +1,68 @@
 package com.crud.rest;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
 
-import org.junit.jupiter.api.Order;
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
-import com.crud.rest.models.dao.PersonaDao;
 import com.crud.rest.models.entity.Persona;
+import com.crud.rest.models.service.IPersonaService;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = Replace.NONE)
 public class CRUDTest {
 	
 	@Autowired
-    private PersonaDao personaDao;
-
+    private IPersonaService personaService;
+	
     @Test
-    @Order(1)
-    public void guardarPersonaTest() {
-        Persona persona = new Persona("Viejo", "20", "Argentina", "viejo@gmail.com", "1163070723", "M", "41778333", "DNI");
-        assertNotNull(personaDao.save(persona));
+    public void buscarPersonas_listaDePersonasExistentes_retornaCincoPersonas() {
+    	List<Persona> personas = personaService.buscarPersonas();
+    	
+    	assertThat(personas).isNotNull();
+    	assertThat(personas.size()).isEqualTo(5);
     }
     
     @Test
-    @Order(2)
-    public void buscarPersonaTest() {
+    public void buscarPersonaPorId_PersonaExistente_retornaUnaPersona() {
     	Long idPersona = 2L;
-    	Persona persona = personaDao.findById(idPersona).orElse(null);
+    	Persona persona = personaService.buscarPersonaPorId(idPersona);
+    	
+    	assertThat(persona).isNotNull();
     	assertThat(persona.getId()).isEqualTo(idPersona);
     }
     
     @Test
-    @Order(3)
-    public void eliminarPersonaTest() {
-    	Long idPersona = 2L;
-    	boolean existeLaPersonaAntesDeEliminar = personaDao.findById(idPersona).isPresent();
-    	personaDao.deleteById(idPersona);
-    	boolean existeLaPersonaDespuesDeEliminar = personaDao.findById(idPersona).isPresent();
-    	assertTrue(existeLaPersonaAntesDeEliminar);
-    	assertFalse(existeLaPersonaDespuesDeEliminar);
+    public void cantidadPersonasPorGenero_personalMasculinoExistente_retornaCincoMasculinos() {
+    	int personalMasculino = personaService.cantidadPersonasPorGenero("M");
+    	
+    	assertThat(personalMasculino).isNotZero();
+    	assertThat(personalMasculino).isNotNegative();
+    	assertThat(personalMasculino).isEqualTo(5);
+    	
     }
+    
+    @Test
+    public void porcentajeDeArgentinos_retornaPorcentajeTotal() {
+    	int porcentaje = personaService.porcentajeDeArgentinos();
+    	
+    	assertThat(porcentaje).isNotZero();
+    	assertThat(porcentaje).isNotNegative();
+    	assertThat(porcentaje).isEqualTo(5);
+    	
+    }
+    
+    @Test
+    public void existeLaPersona_PersonaExistente_retornaPersona() {
+    	String numeroDni = "41778338";
+    	boolean existe = personaService.existenLaPersona(numeroDni);
+    	assertThat(existe).isTrue();
+    }
+    
 
 }
